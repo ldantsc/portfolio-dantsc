344957c02328c52ced06080c13f58d356582e5c6
Lucas Dantas <lucasdantasguitar@gmail.com>
2023-10-28 22:28:45 -0300

    initial commit with new framework

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		container: {
+			padding: {
+			  DEFAULT: '1.8rem',
+			  sm: '3rem',
+			  lg: '6rem',
+			  xl: '7rem',
+			  '2xl': '6rem',
+			},
+		  },
+		extend: {},
+	},
+	plugins: [],
+}
