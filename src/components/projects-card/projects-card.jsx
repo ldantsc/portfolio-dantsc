f45161b3bb328e0f4430372660513a309db41e61
Lucas Dantas <lucasdantasguitar@gmail.com>
2023-11-02 20:41:55 -0300

    test: ui project cards

--- src/components/projects-card/projects-card.jsx
+++ src/components/projects-card/projects-card.jsx
@@ -5,14 +5,16 @@ import style from '../../styles/projects.module.css'
 export default function ProjectsCard(props) {
 
     return (
-        <div id="projects-card" className={style.card}>
-            <div>
-                <img src="https://via.placeholder.com/213x157" alt="" />
+        <div className={style.card}>
+            <div className='mb-1'>
+                <img src={props.image} alt="imagem do projeto" />
             </div>
-            <div>
-                <h3>{props.title}</h3>
-                <p>{props.description}</p>
-                <a href={props.url} target="_blank">Saiba Mais</a>
+            <div className='px-6 grow'>
+                <h3 className='mb-3'>{props.title.replaceAll('-', ' ').toUpperCase()}</h3>
+                <p className='text-sm italic p-2'>{props.description}</p>
+            </div>
+            <div className='mb-6'>
+                <a className={style.btn_url} href={props.url} target="_blank">Saiba Mais</a>
             </div>
         </div>
     )
