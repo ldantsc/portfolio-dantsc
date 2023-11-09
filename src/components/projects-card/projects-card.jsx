b501a073e5a267b63a171db5ebaec8524fbf45fe
Lucas Dantas <dants.dev@gmail.com>
2023-11-09 20:56:09 -0300

    feat: create modal

--- src/components/projects-card/projects-card.jsx
+++ src/components/projects-card/projects-card.jsx
@@ -1,20 +1,17 @@
 import style from '../../styles/projects.module.css'
 
-
 export default function ProjectsCard(props) {
 
     return (
-        <div className={style.card} data-aos="flip-left"
-        data-aos-easing="ease-out-cubic"
-        data-aos-duration="1000" >
-            <div className='mb-1'>
+        <div className={style.card} data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000" >
+            <div className={style.container_card_image}>
                 <img src={props.image} alt="imagem do projeto" />
             </div>
-            <div className='px-3 grow'>
-                <h3 className='my-3'>{props.title.replaceAll('-', ' ').toUpperCase()}</h3>
-                <p className='text-sm italic p-2'>{props.description}</p>
+            <div className={style.container_card_descript}>
+                <h3>{props.title.replaceAll('-', ' ').toUpperCase()}</h3>
+                <p>{props.description}</p>
             </div>
-            <div className='mb-6'>
+            <div className={style.container_btn}>
                 <a className={style.btn_url} href={props.url} target="_blank">Saiba Mais</a>
             </div>
         </div>
