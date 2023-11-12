72d791605ff0714f132b55b4416a057d505fa4b1
Lucas Dantas <dants.dev@gmail.com>
2023-11-12 12:59:04 -0300

    fix: enable vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   output: 'server',
-  adapter: vercel ({
+  adapter: vercel({
     webAnalytics: {
       enable: true,
     }
