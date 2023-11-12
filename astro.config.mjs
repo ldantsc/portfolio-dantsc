aeb64e4b892b95b486a3d69ab6cd11be9a9451f6
Lucas Dantas <dants.dev@gmail.com>
2023-11-12 13:06:38 -0300

    fix: removing vercel import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
-import vercel from '@astrojs/vercel/static';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
